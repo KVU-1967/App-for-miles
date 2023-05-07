e532533eb1e969dc837dc87553699fd4c9eca357
Vladislav Kosenko <katko5@mail.ru>
2023-05-07 17:22:23 +0500

    Сhanged commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public int calculate(int a) {
+        int result;
+        int price = a;
+        int bonuses = 20;
+        result = a / 20;
+        return result;
+    }
+}
